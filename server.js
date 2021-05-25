7fa3a1754bd02f4954b4caebd9a6465fc5b9e5af
Nigelrex <nigelrex2021@gmail.com>
2021-05-25 17:49:42 +0530

    added self ping

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const server = express();
+var ping = require('ping');
 server.all('/', (req, res)=>{
     res.send('Thanks for waking up CraftYourWorld Bot!')
 })
 function keepAlive(){
     server.listen(3000, ()=>{console.log("server running\n")});
 }
+
+
+var hosts = ['https://CraftYourWorld-Bot.nigelrex.repl.co'];
+hosts.forEach(function(host){
+    ping.sys.probe(host, function(isAlive){
+        var msg = isAlive ?  host + ' is up and running\n' : host + ' is down\n';
+        console.log(msg);
+    });
+});
 module.exports = keepAlive;
