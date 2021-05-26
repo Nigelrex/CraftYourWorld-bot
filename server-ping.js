c916de396efe668ecf9fac0b5d5b9c6c1c35f328
Nigelrex <nigelrex2021@gmail.com>
2021-05-26 10:44:45 +0530

    added a date and time system

--- server-ping.js
+++ server-ping.js
@@ -4,13 +4,24 @@ var hosts = ["https://CraftYourWorld-Bot.nigelrex.repl.co"];
 var myVar;
 
 function cywbping() {
+  var d = new Date();
   myVar = setInterval(bping, 300000);
+  var hr = d.getHours();
+  var mi = d.getMinutes();
+  var sec = d.getSeconds();
+  console.log(
+    "Time:" + hr + ":" + mi + ":" + sec
+  );
 }
 function bping() {
   hosts.forEach(function (host) {
     ping.sys.probe(host, function (isAlive) {
+      var d = new Date();
       var msg = isAlive ? host + " is up and running\n" : host + " is down\n";
-      console.log(msg);
+      var hr = d.getHours();
+      var mi = d.getMinutes();
+      var sec = d.getSeconds();
+      console.log("Time:" + hr + ":" + mi + ":" + sec + "Repl "+ msg);
     });
   });
 }
